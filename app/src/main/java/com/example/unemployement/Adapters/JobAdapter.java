f178745ed5c378d6f8d0f548c3e0d288ed4dd0b0
Rudra <rudra.behera50@gmail.com>
2022-02-24 21:46:29 +0530

    JObs and Internships
    Signed-off-by: Rudra <rudra.behera50@gmail.com>

---
+++ app/src/main/java/com/example/unemployement/Adapters/JobAdapter.java
@@ -0,0 +1,86 @@
+package com.example.unemployement.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.unemployement.Models.JobModel;
+import com.example.unemployement.R;
+
+import java.util.ArrayList;
+
+public class JobAdapter extends RecyclerView.Adapter<JobAdapter.JobViewHolder>{
+
+    private Context context;
+    private ArrayList<JobModel> jobs;
+//    private WebinarAdapter.ItemClickListener clickListener;
+
+//    public JobAdapter(Context context, ArrayList<JobModel> foods, ItemClickListener clickListener) {
+//        this.context = context;
+//        this.jobs = jobs;
+//        this.clickListener = clickListener;
+//    }
+
+    public JobAdapter(Context context, ArrayList<JobModel> jobs) {
+        this.context = context;
+        this.jobs = jobs;
+    }
+
+
+    @NonNull
+    @Override
+    public JobViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new JobViewHolder(LayoutInflater.from(context).inflate(R.layout.cell_job, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull JobAdapter.JobViewHolder holder, int position) {
+        JobModel data = jobs.get(position);
+
+        holder.mTvJobTitle.setText(data.getPosition());
+        holder.mTvJobDate.setText(String.valueOf(data.getDuration()));
+        holder.mTvJobPlace.setText(String.valueOf(data.getLocation()));
+        holder.mTvJobCompany.setText(String.valueOf(data.getCompany()));
+
+//        Image Loading
+        Glide.with(this.context).load(data.getThumbnail()).into(holder.mIvJobImg);
+
+//        holder.itemView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                clickListener.onItemClick(webinar.get(position));
+//            }
+//        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return jobs.size();
+    }
+
+    public class JobViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView mIvJobImg;
+        private TextView mTvJobTitle;
+        private TextView mTvJobDate;
+        private TextView mTvJobCompany;
+        private TextView mTvJobPlace;
+
+        public JobViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            mIvJobImg = itemView.findViewById(R.id.iv_jobImage);
+            mTvJobTitle = itemView.findViewById(R.id.tv_jobTitle);
+            mTvJobDate = itemView.findViewById(R.id.tv_exp);
+            mTvJobCompany = itemView.findViewById(R.id.tv_companyName);
+            mTvJobPlace = itemView.findViewById(R.id.tv_jobLocation);
+        }
+    }
+}
--- app/src/main/java/com/example/unemployement/ui/Fragment/SoftskillFragment.java
+++ app/src/main/java/com/example/unemployement/ui/Fragment/SoftskillFragment.java
@@ -18,20 +18,12 @@ import com.example.unemployement.ui.Activity.EntrepreneurshipActivity;
 import com.example.unemployement.ui.Activity.HomeScreen;
 import com.example.unemployement.ui.Activity.MainActivity;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SoftskillFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SoftskillFragment extends Fragment {
     private CardView resume,entrpreneur;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -39,15 +31,6 @@ public class SoftskillFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FaqFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static SoftskillFragment newInstance(String param1, String param2) {
         SoftskillFragment fragment = new SoftskillFragment();
         Bundle args = new Bundle();
@@ -70,7 +53,8 @@ public class SoftskillFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+        View view =  inflater.inflate(R.layout.fragment_softskill, container, false);
+
         resume = view.findViewById(R.id.cv_resume);
         entrpreneur=view.findViewById(R.id.cv_entrepreneur);
 
@@ -81,6 +65,7 @@ public class SoftskillFragment extends Fragment {
 
             }
         });
+
         entrpreneur.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
